09e72edd3be10d1934dabe02b8127c73da1f580e
Hyosssssun <hyosssssun@gmail.com>
2022-03-15 01:51:37 +0000

    header img icon isn't placed center

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -17,14 +17,14 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-      <div className="app__header app__flex">
+      <div id='home' className="app__header app__flex">
           <motion.div
               whileInView={{ x: [-100, 0], opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__header-info"
           >
               <div className="app__header-badge">
-                  <div className="badge-map app__flex">
+                  <div className="badge-cmp app__flex">
                       <span>👋</span>
                       <div style={{ marginLeft: 20 }}>
                           <p className="p-text">Hello, I am</p>
