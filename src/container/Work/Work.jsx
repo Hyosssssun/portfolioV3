86d5a11ed9e231f968a659a467602fa246a07e4c
Hyosssssun <hyosssssun@gmail.com>
2022-03-26 13:15:42 +0000

    added style for portfolio

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -58,17 +58,37 @@ const Work = () => {
               >
                 <a href={work.projectLink} target="_blank" rel='noreferrer'>
                   <motion.div
-                    whileInView={{ scale: [0,1] }}
+                    whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className='app__flex'
                   >
-
+                    <AiFillEye />
                   </motion.div>
                 </a>
-
+                <a href={work.codeLink} target="_blank" rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className='app__flex'
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
               </motion.div>
             </div>
+
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <p className='p-text' style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>{work.tags[0]}</p>
+              </div>
+            </div>
+
+
           </div>
         ))}
       </motion.div>
@@ -76,4 +96,4 @@ const Work = () => {
   );
 }
 
-export default Work
+export default AppWrap(Work, 'work')
