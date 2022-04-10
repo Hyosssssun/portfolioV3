956fbb6e870d79da44619d3a91a04ea67c1ef3f5
Hyosssssun <hyosssssun@gmail.com>
2022-04-10 21:27:03 +0100

    changed design of Project section and added responsive code

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -46,12 +46,17 @@ const Work = () => {
           <div className="app__work-filter">
               {[
                   "All",
-                  "UI/UX",
-                  "Web App",
-                  "Mobile App",
-                  "JavaScript",
+                  "Team",
+                  "Personal",
+                  "HTML",
                   "CSS",
+                  "Sass",
+                  "JavaScript",
+                  "Node JS",
                   "React JS",
+                  "PostgreSQL",
+                  "Express",
+                  "PWA",
               ].map((item, index) => (
                   <div
                       key={index}
@@ -118,13 +123,44 @@ const Work = () => {
 
                       <div className="app__work-content app__flex">
                           <h4 className="bold-text">{work.title}</h4>
-                          <p className="p-text" style={{ marginTop: 10 }}>
+                          <p className="p-text desc" style={{ marginTop: 10 }}>
                               {work.description}
                           </p>
-
-                          <div className="app__work-tag app__flex">
-                              <p className="p-text">{activeFilter}</p>
+                          <div className="mobile-link">
+                              <a
+                                  href={work.projectLink}
+                                  target="_blank"
+                                  rel="noreferrer"
+                              >
+                                  <motion.div
+                                      whileInView={{ scale: [0.7, 1] }}
+                                      whileTap={{ scale: 0.5 }}
+                                      transition={{ duration: 0.25 }}
+                                      className="p-text app__flex"
+                                  >
+                                      <AiFillEye />
+                                      Demo
+                                  </motion.div>
+                              </a>
+                              <a
+                                  href={work.codeLink}
+                                  target="_blank"
+                                  rel="noreferrer"
+                              >
+                                  <motion.div
+                                      whileInView={{ scale: [0.7, 1] }}
+                                      whileTap={{ scale: 0.5 }}
+                                      transition={{ duration: 0.25 }}
+                                      className="p-text app__flex"
+                                  >
+                                      <AiFillGithub />
+                                      Code
+                                  </motion.div>
+                              </a>
                           </div>
+                          {/* <div className="app__work-tag app__flex">
+                              <p className="p-text">{work.tags.join(", ")}</p>
+                          </div> */}
                       </div>
                   </div>
               ))}
