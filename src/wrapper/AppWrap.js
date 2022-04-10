5085913681bd4a3ffdd34795fe9bc52c80ee66b0
Hyosssssun <hyosssssun@gmail.com>
2022-04-10 21:37:07 +0100

    changed year in copyright hard code to getFullYear()

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,33 +1,39 @@
-import React from 'react'
-import { NavigationDots, SocialMedia } from '../components'
+import React from "react";
+import { NavigationDots, SocialMedia } from "../components";
 
-const AppWrap = (Component, idName, classNames) => function HOC() {
-  return (
-      <div
-          id={idName}
-          className={
-              idName === "contact"
-                  ? `app__container-no-min-height ${classNames}`
-                  : `app__container ${classNames}`
-          }
-      >
-          {idName !== "contact" && <SocialMedia />}
+const AppWrap = (Component, idName, classNames) =>
+    function HOC() {
+        const year = new Date().getFullYear();
+        return (
+            <div
+                id={idName}
+                className={
+                    idName === "contact"
+                        ? `app__container-no-min-height ${classNames}`
+                        : `app__container ${classNames}`
+                }
+            >
+                {idName !== "contact" && <SocialMedia />}
 
-          <div className={
-              idName === "contact"
-                  ? 'app__wrapper-no-padding app_flex'
-                  :  idName === "about" ? 'app__wrapper-no-padding app_flex' : 'app__wrapper app_flex'
-          }>
-              <Component />
+                <div
+                    className={
+                        idName === "contact"
+                            ? "app__wrapper-no-padding app_flex"
+                            : idName === "about"
+                            ? "app__wrapper-no-padding app_flex"
+                            : "app__wrapper app_flex"
+                    }
+                >
+                    <Component />
 
-              <div className="copyright">
-                  <p className="p-text">@2022 Hyosun</p>
-                  <p className="p-text">All rights reserved</p>
-              </div>
-          </div>
-          <NavigationDots active={idName} />
-      </div>
-  );
-}
+                    <div className="copyright">
+                        <p className="p-text">@{year} Hyosun</p>
+                        <p className="p-text">All rights reserved</p>
+                    </div>
+                </div>
+                <NavigationDots active={idName} />
+            </div>
+        );
+    };
 
-export default AppWrap
+export default AppWrap;
