b779f518eccbc312502e9c80cb1131f6d2f68d3a
Hyosssssun <hyosssssun@gmail.com>
2024-12-14 15:14:48 +0000

    lint

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const NavigationDots = ({ active }) => {
-    return (
-        <div className="app__navigation">
-            {["home", "about", "projects", "contact"].map((item, index) => (
-                <a
-                    href={`#${item}`}
-                    key={item + index}
-                    className="app__navigation-dot"
-                    style={
-                        active === item ? { backgroundColor: "#F89B47" } : {}
-                    }
-                >
-                    {}
-                </a>
-            ))}
-        </div>
-    );
-}
+  return (
+    <div className='app__navigation'>
+      {["home", "about", "projects", "contact"].map((item, index) => (
+        <a
+          href={`#${item}`}
+          key={item + index}
+          className='app__navigation-dot'
+          style={active === item ? { backgroundColor: "#F89B47" } : {}}
+        >
+          {}
+        </a>
+      ))}
+    </div>
+  );
+};
 
-export default NavigationDots
+export default NavigationDots;
